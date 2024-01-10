d499aa537974f82da2d9e9b2f31aa002b9d287a9
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-01-10 13:54:38 -0500

    first commit

---
+++ spam.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+curl 'https://www.hhposall.xyz/php/app/index/verify-info.php?t=1704912499601' \
+  -H 'sec-ch-ua: "Not_A Brand";v="8", "Chromium";v="120"' \
+  -H 'DNT: 1' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36' \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -H 'Accept: application/json, text/plain, */*' \
+  -H 'Referer: https://www.usavdfaadf.xyz/' \
+  -H 'sec-ch-ua-platform: "macOS"' \
+  --data-raw 'murmur=51e52efbde60cca78bf9d50d42b97887&uid=22429&first_name=2&last_name=2&phone=222-222-2222&email=2%402.2&address=2&city=2&zip=22222&state=AL' \
+  --compressed
